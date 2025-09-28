ffea5859224dcd3e45e16f1cb6949d4669fbe0b8
PARTH KAVAD <kavadparth54@gmail.com>
2025-09-28 22:29:21 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,40 @@
+//require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+connectDB()
+
+
+
+/* import mongoose from "mongoose";
+import { DB_NAME } from "./constants"; */
+/* 1st apprroch
+import express from "express";
+import { error } from "console";
+const app = express();
+
+;( async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_NAME}/${DB_NAME}`);
+       app.on("error",(error)=>{
+        console.log("ERROR",error);
+        throw error;
+       });
+
+       app.listen(process.env.PORT,()=>{
+        comsole.log(`port is listeing on port ${process.env.PORT}`);
+       })
+    } catch (error) {
+        console.error("ERROR",error);
+        throw error;
+    }
+})()
+
+*/
